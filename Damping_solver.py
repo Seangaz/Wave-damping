15864867120a1ebc622e25d6053e3a4078b18fef
Sean <97883869+Seangaz@users.noreply.github.com>
2023-08-31 23:18:44 +0200

    Update Damping_solver.py

--- Damping_solver.py
+++ Damping_solver.py
@@ -5,7 +5,7 @@ L =  0
 f = 0
 mu =  0
 def solve(ζ):
-    return (np.pi * np.sqrt(1 - ζ**2)) / 4 - ζ + T / (8 * mu * f**2 * L**2)
+    return ((np.sqrt(1 - ζ**2)*T)/(8*mu*f**2*L**2)+np.pi/4-(ζ**2*np.pi)/4-ζ)
 initial_guess = 0.1
 damping_ratio = fsolve(solve, initial_guess)
 print("Calculated damping ratio ζ =", damping_ratio[0])
